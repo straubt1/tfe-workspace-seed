b92f4ded022e88408114c6126b7c33fcfbed5714
Tom Straub <tstraub@hashicorp.com>
2020-04-06 08:34:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,13 @@ variable "last_ws_mod_date" {
   default = ""
 }
 
+variable "pet_prefix" {
+  default = ""
+}
+
 resource "random_pet" "main" {
   length = 3
+  prefix = var.pet_prefix
 }
 
 output "date" {
