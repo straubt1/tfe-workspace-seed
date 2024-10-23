368ff00e57356592b18babc184cfe9ee6a1de6c4
Tom Straub <tstraub@hashicorp.com>
2024-10-23 13:33:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 variable "test_var" {
   description  = "Dummy variable that can be used to force a variable error."
   type         = string
-  default      = 9
+  default      = 7
 }
 
 # Generate a change in the workspace
